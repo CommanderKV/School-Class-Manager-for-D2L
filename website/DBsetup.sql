7378049b6d7b286defff518107e4405ee101a96c
CommanderKV <kylervisser@gmail.com>
2024-09-22 17:11:27 -0400

    Updated various different quires in testing so they work

--- website/DBsetup.sql
+++ website/DBsetup.sql
@@ -1,5 +1,5 @@
 -- Make database
---CREATE DATABASE classes;
+-- CREATE DATABASE classes;
 USE classes;
 
 DROP TABLE IF EXISTS 
@@ -12,9 +12,8 @@ DROP TABLE IF EXISTS
 `Attachments`, 
 `Classes`, 
 `Feedback`, 
-`Submissions`, 
-`Users`, 
-`UsersToClasses`;
+`Submissions`;
+-- `Users`,
 
 -- Make tables
 CREATE TABLE Users (
@@ -30,22 +29,15 @@ CREATE TABLE Users (
 
 CREATE TABLE Classes (
     classID INT AUTO_INCREMENT PRIMARY KEY,
+    userID INT NOT NULL,
     closed BOOLEAN NOT NULL DEFAULT FALSE,
     link VARCHAR(255) NOT NULL,
     name VARCHAR(100) NOT NULL,
     courseCode VARCHAR(100) NOT NULL,
     termShort VARCHAR(20) NOT NULL,
-    termLong VARCHAR(100) NOT NULL
+    termLong VARCHAR(100) NOT NULL,
+    FOREIGN KEY (userID) REFERENCES Users(userID)
 ) AUTO_INCREMENT = 1000;
-
-
-CREATE TABLE UsersToClasses (
-    linkID INT AUTO_INCREMENT PRIMARY KEY,
-    userID INT NOT NULL,
-    classID INT NOT NULL,
-    FOREIGN KEY (userID) REFERENCES Users(userID),
-    FOREIGN KEY (classID) REFERENCES Classes(classID)
-);
 
 CREATE TABLE Assignments (
     assignmentID INT AUTO_INCREMENT PRIMARY KEY,
@@ -61,22 +53,18 @@ CREATE TABLE Assignments (
 CREATE TABLE Submissions (
     submissionID INT AUTO_INCREMENT PRIMARY KEY,
     assignmentID INT NOT NULL,
-    userID INT NOT NULL,
     comment VARCHAR(1000) DEFAULT NULL,
     d2lSubmissionID INT NOT NULL,
     date DATETIME DEFAULT NULL,
-    FOREIGN KEY (assignmentID) REFERENCES Assignments(assignmentID),
-    FOREIGN KEY (userID) REFERENCES Users(userID)
+    FOREIGN KEY (assignmentID) REFERENCES Assignments(assignmentID)
 ) AUTO_INCREMENT = 1;
 
 CREATE TABLE Feedback (
     feedbackID INT AUTO_INCREMENT PRIMARY KEY,
-    userID INT NOT NULL,
     submissionID INT NOT NULL,
     html VARCHAR(255) DEFAULT NULL,
     date DATETIME NOT NULL,
-    FOREIGN KEY (submissionID) REFERENCES Submissions(submissionID),
-    FOREIGN KEY (userID) REFERENCES Users(userID)
+    FOREIGN KEY (submissionID) REFERENCES Submissions(submissionID)
 ) AUTO_INCREMENT = 1;
 
 CREATE TABLE Attachments (
@@ -142,12 +130,16 @@ SELECT
             'attachments', AssignmentAttachments.attachmentsJSON,   -- Assignment attachments
             'submissions', AssignmentSubmissions.submissionsJSON,   -- Assignment submissions
             'feedback', AssignmentsFeedback.feedbackJSON,           -- Assignment feedback
-            'submissionURL', Assignments.submissionURL              -- Assignment submissionURL
+            'submissionURL', Assignments.submissionURL,             -- Assignment submissionURL
+            'grade', Grades.grade,
+            'weight', Grades.weight
         )
     ) AS assignments
 FROM Classes                                                        -- Get the classes details
 JOIN Assignments ON Classes.classID = Assignments.classID           -- Get the assignments details
-LEFT JOIN (                                                                             -- Get the assignments attachments
+LEFT JOIN GradesLinkToAssignments ON Assignments.assignmentID = GradesLinkToAssignments.assignmentID
+LEFT JOIN Grades ON GradesLinkToAssignments.gradeID = Grades.gradeID
+LEFT JOIN (                                                         -- Get the assignments attachments
     SELECT 
         Assignments.assignmentID,           -- Assignment ID
         JSON_ARRAYAGG(
@@ -159,12 +151,12 @@ LEFT JOIN (                                                                     
         ) AS attachmentsJSON
     FROM Attachments                                                                                        -- Get the attachments details
     JOIN AttachmentLinkToAssignment ON Attachments.attachmentID = AttachmentLinkToAssignment.attachmentID   -- Get the attachment link to assignment
-    JOIN Assignments ON AttachmentLinkToAssignment.assignmentID = Assignments.assignmentID                  -- Get the assignments details   
+    JOIN Assignments ON AttachmentLinkToAssignment.assignmentID = Assignments.assignmentID                  -- Get the assignments details
     GROUP BY Assignments.assignmentID                                                                       -- Group by assignment ID 
 ) AS AssignmentAttachments ON Assignments.assignmentID = AssignmentAttachments.assignmentID                 -- Assign the attachments to AssignmentAttachments
 LEFT JOIN (                                                                                                 -- Get the assignments submissions
     SELECT 
-        Submissions.assignmentID,                                       -- Assignment ID
+        DISTINCT Submissions.assignmentID,                                       -- Assignment ID
         JSON_ARRAYAGG(
             JSON_OBJECT(
                 'd2lSubmissionID', Submissions.d2lSubmissionID,         -- Submission ID
@@ -176,7 +168,7 @@ LEFT JOIN (                                                                     
     FROM Submissions                                                    -- Get the submissions details
     LEFT JOIN (
         SELECT 
-            AttachmentLinkToSubmission.submissionID,    -- Submission ID
+            DISTINCT AttachmentLinkToSubmission.submissionID,    -- Submission ID
             JSON_ARRAYAGG(
                 JSON_OBJECT(
                     'link', Attachments.link,           -- Attachment link
@@ -192,7 +184,7 @@ LEFT JOIN (                                                                     
 ) AS AssignmentSubmissions ON Assignments.assignmentID = AssignmentSubmissions.assignmentID                     -- Assign the submissions to AssignmentSubmissions
 LEFT JOIN (                                                                                                     -- Get the assignments feedback
     SELECT 
-        Assignments.assignmentID,       -- Assignment ID
+        DISTINCT Assignments.assignmentID,       -- Assignment ID
         JSON_ARRAYAGG(
             JSON_OBJECT(
                 'html', Feedback.html,  -- Feedback html
@@ -204,9 +196,7 @@ LEFT JOIN (                                                                     
     JOIN Assignments ON Submissions.assignmentID = Assignments.assignmentID                 -- Get the assignments details
     GROUP BY Assignments.assignmentID                                                       -- Group by assignment ID  
 ) AS AssignmentsFeedback ON AssignmentsFeedback.assignmentID = Assignments.assignmentID     -- Assign the feedback to AssignmentsFeedback
-INNER JOIN UsersToClasses ON Classes.classID = UsersToClasses.classID                       -- Get the users to classes link
-INNER JOIN Users ON UsersToClasses.userID = Users.userID                                    -- Get users
-WHERE Users.userID = 1000                                                                   -- Filter by user ID
+WHERE Classes.userID = 1000
 GROUP BY Classes.classID;                                                  -- Group by class ID and ClassGrade.grade
 
 
@@ -216,6 +206,8 @@ SELECT * FROM Classes;
 SELECT * FROM Attachments;
 
 SELECT * FROM Assignments;
+
+SELECT * FROM Assignments WHERE classID = 1002;
 
 SELECT * FROM Submissions;
 
@@ -229,6 +221,20 @@ SELECT * FROM UsersToClasses;
 
 SELECT LAST_INSERT_ID();
 
+WITH DuplicateGrades AS (
+    SELECT 
+        gla.assignmentID,
+        gla.gradeID,
+        ROW_NUMBER() OVER (PARTITION BY gla.assignmentID ORDER BY gla.gradeID ASC) AS rn
+    FROM GradesLinkToAssignments gla
+)
+DELETE FROM GradesLinkToAssignments
+WHERE gradeID IN (
+    SELECT gradeID 
+    FROM DuplicateGrades 
+    WHERE rn > 1  -- Keep only the lowest ID (rn = 1)
+);
+
 
 SELECT 
     Grades.grade,
@@ -241,8 +247,16 @@ LEFT JOIN GradesAssignmentsLinkToClasses ON
 LEFT JOIN Users ON Grades.userID = Users.userID
 WHERE 
     Grades.UserID = 1000 AND 
-    GradesLinkToAssignments.assignmentID = 1
+    GradesLinkToAssignments.assignmentID = 56
 GROUP BY Grades.gradeID;
+
+SELECT
+    Grades.grade,
+    Grades.weight
+FROM Grades
+LEFT JOIN GradesLinkToAssignments ON Grades.gradeID = GradesLinkToAssignments.gradeID
+LEFT JOIN Assignments ON GradesLinkToAssignments.assignmentID = Assignments.assignmentID
+WHERE Assignments.assignmentID = 2
 
 
 SELECT 
@@ -327,7 +341,165 @@ LEFT JOIN (
 ) AS AssignmentsFeedback ON AssignmentsFeedback.assignmentID = Assignments.assignmentID
 INNER JOIN UsersToClasses ON Classes.classID = UsersToClasses.classID
 INNER JOIN Users ON UsersToClasses.userID = Users.userID
-WHERE Users.userID = 1000
+WHERE Users.userID = 1001
+GROUP BY Classes.classID;
+
+SELECT
+    Classes.name AS className,
+    Classes.courseCode,
+    Classes.link AS classLink,
+    Classes.termShort,
+    Classes.closed,
+    JSON_ARRAYAGG(
+        DISTINCT JSON_OBJECT(
+            'name', Assignments.name,
+            'link', Assignments.link,
+            'dueDate', Assignments.dueDate,
+            'instructions', Assignments.instructions,
+            'attachments', (
+                SELECT JSON_ARRAYAGG(
+                    DISTINCT JSON_OBJECT(
+                        'link', Attachments.link,
+                        'size', Attachments.size,
+                        'name', Attachments.name
+                    )
+                )
+                FROM Attachments
+                JOIN AttachmentLinkToAssignment ON Attachments.attachmentID = AttachmentLinkToAssignment.attachmentID
+                WHERE AttachmentLinkToAssignment.assignmentID = Assignments.assignmentID
+            ),
+            'submissions', (
+                SELECT JSON_ARRAYAGG(
+                    DISTINCT JSON_OBJECT(
+                        'd2lSubmissionID', Submissions.d2lSubmissionID,
+                        'comment', Submissions.comment,
+                        'date', Submissions.date,
+                        'attachments', (
+                            SELECT JSON_ARRAYAGG(
+                                JSON_OBJECT(
+                                    'link', Attachments.link,
+                                    'size', Attachments.size,
+                                    'name', Attachments.name
+                                )
+                            )
+                            FROM Attachments
+                            JOIN AttachmentLinkToSubmission ON Attachments.attachmentID = AttachmentLinkToSubmission.attachmentID
+                            WHERE AttachmentLinkToSubmission.submissionID = Submissions.submissionID
+                        )
+                    )
+                )
+                FROM Submissions
+                WHERE Submissions.assignmentID = Assignments.assignmentID
+            ),
+            'feedback', (
+                SELECT JSON_ARRAYAGG(
+                    DISTINCT JSON_OBJECT(
+                        'html', Feedback.html,
+                        'date', Feedback.date
+                    )
+                )
+                FROM Feedback
+                JOIN Submissions ON Feedback.submissionID = Submissions.submissionID
+                WHERE Submissions.assignmentID = Assignments.assignmentID
+            ),
+            'submissionURL', Assignments.submissionURL,
+            'grade', Grades.grade,
+            'weight', Grades.weight
+        )
+    ) AS assignments
+FROM Classes
+JOIN Assignments ON Classes.classID = Assignments.classID 
+LEFT JOIN GradesLinkToAssignments ON Assignments.assignmentID = GradesLinkToAssignments.assignmentID
+LEFT JOIN Grades ON GradesLinkToAssignments.gradeID = Grades.gradeID
+WHERE Classes.userID = 1000
 GROUP BY Classes.classID;
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+USE classes;
+SELECT
+    Classes.name as className,
+    Classes.courseCode,
+    Classes.link AS classLink,
+    Classes.termShort,
+    Classes.closed,
+    (
+        SELECT
+            JSON_ARRAYAGG(
+                JSON_OBJECT(
+                    'name', distinctAssignments.name,
+                    'link', distinctAssignments.link,
+                    'dueDate', distinctAssignments.dueDate,
+                    'instructions', distinctAssignments.instructions,
+                    'attachments', (
+                        SELECT JSON_ARRAYAGG(
+                            JSON_OBJECT(
+                                'link', Attachments.link,
+                                'size', Attachments.size,
+                                'name', Attachments.name
+                            )
+                        ) FROM Attachments
+                        JOIN AttachmentLinkToAssignment ON Attachments.attachmentID = AttachmentLinkToAssignment.attachmentID
+                        WHERE AttachmentLinkToAssignment.assignmentID = distinctAssignments.assignmentID
+                    ),
+                    'submissions', (
+                        SELECT JSON_ARRAYAGG(
+                            JSON_OBJECT(
+                                'd2lSubmissionID', Submissions.d2lSubmissionID,
+                                'comment', Submissions.comment,
+                                'date', Submissions.date,
+                                'attachments', (
+                                    SELECT JSON_ARRAYAGG(
+                                        JSON_OBJECT(
+                                            'link', Attachments.link,
+                                            'size', Attachments.size,
+                                            'name', Attachments.name
+                                        )
+                                    )
+                                    FROM Attachments
+                                    JOIN AttachmentLinkToSubmission ON Attachments.attachmentID = AttachmentLinkToSubmission.attachmentID
+                                    WHERE AttachmentLinkToSubmission.submissionID = Submissions.submissionID
+                                )
+                            )
+                        )
+                        FROM Submissions
+                        WHERE Submissions.assignmentID = distinctAssignments.assignmentID
+                    ),
+                    'feedback', (
+                        SELECT JSON_ARRAYAGG(
+                            JSON_OBJECT(
+                                'html', Feedback.html,
+                                'date', Feedback.date
+                            )
+                        )
+                        FROM Feedback
+                        JOIN Submissions ON Feedback.submissionID = Submissions.submissionID
+                        WHERE Submissions.assignmentID = distinctAssignments.assignmentID
+                    ),
+                    'submissionURL', distinctAssignments.submissionURL,
+                    'grade', Grades.grade,
+                    'weight', Grades.weight
+                )
+            )
+        FROM (
+            SELECT DISTINCT Assignments.*
+            FROM Assignments
+        ) as distinctAssignments
+        LEFT JOIN GradesLinkToAssignments ON distinctAssignments.assignmentID = GradesLinkToAssignments.assignmentID
+        LEFT JOIN Grades ON GradesLinkToAssignments.gradeID = Grades.gradeID
+        WHERE distinctAssignments.classID = Classes.classID
+    ) AS assignments
+FROM Classes
+WHERE Classes.userID = 1000
+GROUP BY Classes.classID;
