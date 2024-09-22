3c8ed91b5ef8cf0d063954ac2e7adc61d2be6e8e
CommanderKV <kylervisser@gmail.com>
2024-09-22 18:58:53 -0400

    Updated to solve pylint error again maybe

--- courseScraper/course.py
+++ courseScraper/course.py
@@ -49,7 +49,19 @@ class Course:
         - assignments (list[Assignment]|None): 
             The list of assignments of the course.
     """
-    def __init__(self, baseURL: str, link: str="", closed: bool=False, shortTerm: str="", courseCode: str="", name: str="", longTerm: str="", syllabus: str|None="", assignmentsURL: str="", assignments: list[Assignment]|None=[]): # pylint: disable=dangerous-default-value, too-many-arguments, too-many-function-args
+    def __init__(
+            self,
+            baseURL: str,
+            link: str="",
+            closed: bool=False,
+            shortTerm: str="",
+            courseCode: str="",
+            name: str="",
+            longTerm: str="",
+            syllabus: str|None="",
+            assignmentsURL: str="",
+            assignments: list[Assignment]|None=[]
+        ): # pylint: disable=dangerous-default-value, too-many-positional-arguments
         """
         # Description:
             The constructor of the Course class.
